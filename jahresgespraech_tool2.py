225b8f9864cd675203b36ffeb24f908f3559b155
littmann15 <Info@leinekompetenz.de>
2025-04-19 08:35:07 +0200

    Add files via upload

---
+++ jahresgespraech_tool2.py
@@ -0,0 +1,3 @@
+# Achtung: Dies ist ein Platzhalter und enthält React/JSX-Code als Text.
+# Die eigentliche Anwendung basiert auf React (Frontend), nicht auf Python (Backend).
+# Diese Datei dient ggf. der referenziellen Archivierung oder Vorbereitung zur Integration in ein Flask-/Django-Projekt.
